125fcf8003ec80d551cedd89ef710c8db3067586
joshpickardme <hello@joshpickard.me>
2023-10-26 14:42:41 +0100

    User.js complete

--- src/User.js
+++ src/User.js
@@ -1,5 +1,27 @@
 class User {
-  // User code here
+  #loggedIn
+  constructor(user, password, age) {
+    this.user = user;
+    this.password = password;
+    this.age = age;
+    this.#loggedIn = false;
+  }
+  login(password) {
+      if(password === this.password) {
+        this.#loggedIn = true
+        console.log("user logged in")
+      } else {
+        throw new Error("Incorrect Password Entered")
+      }
+  }
+  logout() {
+    this.#loggedIn = false;
+  }
 }
+
+
+const newUser = new User("Josh", "123", 21)
+
+
 
 module.exports = User
